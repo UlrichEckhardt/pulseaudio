c2add54eda70398d307b1c9faf6965aa776bd731
Ulrich Eckhardt <ulrich.eckhardt@base-42.de>
2016-05-15 15:54:19 +0200

    modargs-test: Add tests for pa_modargs_get_value()

--- src/tests/modargs-test.c
+++ src/tests/modargs-test.c
@@ -50,6 +50,66 @@ START_TEST (modargs_test_empty_string) {
 }
 END_TEST
 
+START_TEST (modargs_test_get_value_1) {
+    const char* keys[] = {
+        "abc",
+        NULL
+    };
+    pa_modargs *args;
+    const char* value;
+
+    args = pa_modargs_new("", keys);
+    ck_assert_ptr_ne(args, NULL);
+
+    /* default value handling */
+    value = pa_modargs_get_value(args, "abc", NULL);
+    ck_assert_ptr_eq(value, NULL);
+
+    /* default value handling */
+    value = pa_modargs_get_value(args, "abc", "default value");
+    ck_assert_str_eq(value, "default value");
+
+    /* unknown key handling */
+    value = pa_modargs_get_value(args, "def", "default value");
+    ck_assert_str_eq(value, "default value");
+
+    pa_modargs_free(args);
+}
+END_TEST
+
+START_TEST (modargs_test_get_value_2) {
+    const char* keys[] = {
+        "abc",
+        "def",
+        "ghi",
+        NULL
+    };
+    pa_modargs *args;
+    const char* value;
+
+    args = pa_modargs_new("abc=123 def=xyz", keys);
+    ck_assert_ptr_ne(args, NULL);
+
+    /* regular value handling */
+    value = pa_modargs_get_value(args, "abc", "default value");
+    ck_assert_str_eq(value, "123");
+
+    /* regular value handling */
+    value = pa_modargs_get_value(args, "def", "default value");
+    ck_assert_str_eq(value, "xyz");
+
+    /* default value handling */
+    value = pa_modargs_get_value(args, "ghi", "default value");
+    ck_assert_str_eq(value, "default value");
+
+    /* unknown key handling */
+    value = pa_modargs_get_value(args, "jkl", "default value");
+    ck_assert_str_eq(value, "default value");
+
+    pa_modargs_free(args);
+}
+END_TEST
+
 int main(int argc, char *argv[]) {
     int failed = 0;
     Suite *s;
@@ -63,6 +123,8 @@ int main(int argc, char *argv[]) {
     tc = tcase_create("modargs");
     tcase_add_test(tc, modargs_test_null);
     tcase_add_test(tc, modargs_test_empty_string);
+    tcase_add_test(tc, modargs_test_get_value_1);
+    tcase_add_test(tc, modargs_test_get_value_2);
     suite_add_tcase(s, tc);
 
     sr = srunner_create(s);
